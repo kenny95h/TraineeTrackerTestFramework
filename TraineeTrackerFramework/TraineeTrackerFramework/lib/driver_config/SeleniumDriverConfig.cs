31e93b752e0372a7cb17f10680b506cbe41c6739
boopidoop <daniel.summerside@yahoo.com>
2022-08-17 16:11:43 +0100

    Initial Skeleton Framework

---
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/driver_config/SeleniumDriverConfig.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace TraineeTrackerFramework.lib.driver_config;
+
+public class SeleniumDriverConfig<T> where T : IWebDriver, new()
+{
+    public IWebDriver Driver { get; set; }
+
+    public SeleniumDriverConfig(int pageLoadSecs, int implicitWaitSecs, bool isHeadless)
+    {
+        Driver = new T();
+        DriverSetUp(pageLoadSecs, implicitWaitSecs, isHeadless);
+    }
+
+    private void DriverSetUp(int pageLoadSecs, int implicitWaitSecs, bool isHeadless)
+    {
+        Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(pageLoadSecs);
+        Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(implicitWaitSecs);
+        if (isHeadless) SetHeadless();
+    }
+
+    private void SetHeadless()
+    {
+        if (Driver is ChromeDriver)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+            Driver.Dispose();
+            Driver = new ChromeDriver(options);
+        }
+        else if (Driver is FirefoxDriver)
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.AddArgument("--headless");
+            Driver.Dispose();
+            Driver = new FirefoxDriver(options);
+        }
+        else
+        {
+            throw new ArgumentException("Browser not supported by framework");
+        }
+    }
+}
---
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/TT_LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeTrackerFramework.lib.pages;
+
+public class TT_LoginPage
+{
+    #region Properties and Fields
+    private IWebDriver _seleniumDriver;
+    private string _loginPageURL = AppConfigReader.LoginURL;
+    private IWebElement _usernameField => _seleniumDriver.FindElement(By.Id("Credential_UserName"));
+    private IWebElement _passwordField => _seleniumDriver.FindElement(By.Id("Credential_Password"));
+    private IWebElement _loginButton => _seleniumDriver.FindElement(By.Id("!!!!!!!!!!FILL ME IN!!!!!!!!!!!"));
+    private IWebElement _usernameAlert => _seleniumDriver.FindElement(By.Id("!!!!!!!!!!FILL ME IN!!!!!!!!!!!"));
+    private IWebElement _passwordAlert => _seleniumDriver.FindElement(By.Id("!!!!!!!!!!FILL ME IN!!!!!!!!!!!"));
+    private IWebElement _spartaLogo => _seleniumDriver.FindElement(By.Id("!!!!!!!!!!FILL ME IN!!!!!!!!!!!"));
+    private IWebElement _privacyPolicyLink => _seleniumDriver.FindElement(By.Id("!!!!!!!!!!FILL ME IN!!!!!!!!!!!"));
+    #endregion
+
+    public TT_LoginPage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+    #region Methods
+    public void VisitLoginPage() => _seleniumDriver.Navigate().GoToUrl(_loginPageURL);
+    public void InputUsername(string username) => _usernameField.SendKeys(username);
+    public void InputPassword(string password) => _passwordField.SendKeys(password);
+    public void ClickSignIn() => _loginButton.Click();
+    public string GetUsernameAlertText() => _usernameAlert.Text;
+    public string GetPasswordAlertText() => _passwordAlert.Text;
+    public void ClickSpartaLogo() => _spartaLogo.Click();
+    public void ClickPrivacyPolicy() => _privacyPolicyLink.Click();
+    #endregion
+}
