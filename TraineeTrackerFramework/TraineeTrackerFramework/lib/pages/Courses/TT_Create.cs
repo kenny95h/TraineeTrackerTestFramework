dd174b1b276b6acadf91e3f1269597be85824aa1
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 11:02:27 +0100

    Completed POM for creation of course, and for the error page.

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Create.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Create.cs
@@ -12,12 +12,20 @@ namespace TraineeTrackerFramework.lib.pages.Courses
         #region regions & properties
         private IWebDriver _seleniumDriver;
         private IWebElement _prevLink => _seleniumDriver.FindElement(By.Id("prev_link"));
+        private IWebElement _nameInput => _seleniumDriver.FindElement(By.Id("name_input"));
+        private IWebElement _startDateInput => _seleniumDriver.FindElement(By.Id("start_date_input"));
+        private IWebElement _weeksLongInput => _seleniumDriver.FindElement(By.Id("weeks_long_input"));
+        private IWebElement _submitBtn => _seleniumDriver.FindElement(By.Id("submit_button"));
         #endregion
 
         public TT_Create(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
 
         #region methods
-
+        public void GoBack() => _prevLink.Click();
+        public void NameInput(string courseName) => _nameInput.SendKeys(courseName);
+        public void StartDateInput(string date) => _startDateInput.SendKeys(date);
+        public void WeeksLongInput(string weekLength) => _weeksLongInput.SendKeys(weekLength);
+        public void Submit() => _submitBtn.Click();
         #endregion
     }
 }
