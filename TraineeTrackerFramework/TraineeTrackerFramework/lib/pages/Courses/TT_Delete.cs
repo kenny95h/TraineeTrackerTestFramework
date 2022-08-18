f50e6f8e8faef67c2359a59e2d76e5160e2407cc
JonOfScience <jdcrofts31@gmail.com>
2022-08-18 10:14:34 +0100

    Courses - Details complete

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Delete.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Delete.cs
@@ -10,18 +10,17 @@ namespace TraineeTrackerFramework.lib.pages.Courses
         private IWebElement _courseStartDate => _seleniumDriver.FindElement(By.Id("coruse_start_date"));
         private IWebElement _courseLengthWeeks => _seleniumDriver.FindElement(By.Id("course_weeks_long"));
         private IWebElement _deleteButton => _seleniumDriver.FindElement(By.Id("delete_button"));
-        private IWebElement _spartaLogo => _seleniumDriver.FindElement(By.Id("logo"));
-        private IWebElement _privacyPolicyLink => _seleniumDriver.FindElement(By.Id("privacy_link"));
+        private IWebElement _previousLink => _seleniumDriver.FindElement(By.Id("prev_link"));
         #endregion
 
         public TT_CoursesDelete(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
 
-
+        #region Methods
         public string GetCourseName() => _courseName.Text;
         public string GetCourseStartDate() => _courseStartDate.Text;
         public string GetCourseDurationWeeks() => _courseLengthWeeks.Text;
         public void ClickDeleteButton() => _deleteButton.Click();
-        public void ClickSpartaLogo() => _spartaLogo.Click();
-        public void ClickPrivacyPolicy() => _privacyPolicyLink.Click();
+        public void ClickPreviousLink() => _previousLink.Click();
+        #endregion
     }
 }
--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Details.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Details.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
 
 namespace TraineeTrackerFramework.lib.pages.Courses
 {
-    internal class TT_Details
+    public class TT_CoursesDetails
     {
+        #region Properties and Fields
+        private IWebDriver _seleniumDriver;
+        private IWebElement _courseName => _seleniumDriver.FindElement(By.Id("course_name"));
+        private IWebElement _courseStartDate => _seleniumDriver.FindElement(By.Id("coruse_start_date"));
+        private IWebElement _courseDurationWeeks => _seleniumDriver.FindElement(By.Id("course_weeks_long"));
+        private IWebElement _editLink => _seleniumDriver.FindElement(By.Id("edit_link"));
+        private IWebElement _previousLink => _seleniumDriver.FindElement(By.Id("prev_link"));
+        #endregion
+
+        public TT_CoursesDetails(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        #region Methods
+        public string GetCourseName() => _courseName.Text;
+        public string GetCourseStartDate() => _courseStartDate.Text;
+        public string GetCourseDurationWeeks() => _courseDurationWeeks.Text;
+        public void ClickEditLink() => _editLink.Click();
+        public void ClickPreviousLink() => _previousLink.Click();
+        #endregion
+
     }
 }
