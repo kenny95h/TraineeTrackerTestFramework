49e2a2dc3c1ceb117f2269f6b9448c92ed0e52df
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 12:25:49 +0100

    Sorted out the index and pages

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Account/TT_AccessDenied.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Account/TT_AccessDenied.cs
@@ -9,18 +9,5 @@ namespace TraineeTrackerFramework.lib.pages.Account
 {
     public class TT_AccessDenied
     {
-        #region properties & fields
-        private IWebDriver _seleniumDriver;
-        private IWebElement _backBtn => _seleniumDriver.FindElement(By.Id("prev_link"));
-        private IWebElement _homeLink => _seleniumDriver.FindElement(By.Id("home_link"));
-        #endregion
-
-        public TT_AccessDenied(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
-
-        #region methods
-        public void ReturnBack() => _backBtn.Click();
-        public void HomePage() => _homeLink.Click();
-        public string GetPageTitle() => _seleniumDriver.Title;
-        #endregion
     }
 }
