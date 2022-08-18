3d9ad156b527b1d7cc0dc7eb5d801ed253dd6721
LabaDev <labalimbu360@gmail.com>
2022-08-18 11:15:30 +0100

    Added trainee create, tracker index+view

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Trackers/TT_Index.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Trackers/TT_Index.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using OpenQA.Selenium;
 namespace TraineeTrackerFramework.lib.pages.Trackers
 {
-    internal class TT_Index
+    public class TT_Index
     {
+        #region Properties and Fields
+        private IWebDriver _seleniumDriver;
+        private IWebElement _quote => _seleniumDriver.FindElement(By.ClassName("mt-4"));
+        #endregion
+        public TT_Index(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+
+
+
     }
 }
--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Trackers/TT_View_Trainer.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Trackers/TT_View_Trainer.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
 
 namespace TraineeTrackerFramework.lib.pages.Trackers
 {
-    internal class TT_View_Trainer
+    public class TT_View_Trainer
     {
+        #region Properties and Fields
+        private IWebDriver _seleniumDriver;
+        private IWebElement _fullName => _seleniumDriver.FindElement(By.ClassName("col-sm-9"));
+        private IWebElement _email => _seleniumDriver.FindElement(By.TagName("col-sm-9"));
+
+
+        #endregion
+
+        public TT_View_Trainer(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        #region Methods
+        public void GetFullName() => _fullName.Displayed.ToString();
+        public void GetEmail() => _email.Displayed.ToString();
+        #endregion
+
     }
 }
