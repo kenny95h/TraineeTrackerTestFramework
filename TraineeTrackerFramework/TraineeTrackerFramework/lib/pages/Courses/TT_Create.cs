550c20610d360b373e36494e90f7e8cffc90df3e
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 09:50:14 +0100

    Started Create Course POM PAGE and Completed ACCOUNT pom page.

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Create.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Courses/TT_Create.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace TraineeTrackerFramework.lib.pages.Courses
 {
-    internal class TT_Create
+    public class TT_Create
     {
+        #region regions & properties
+        private IWebDriver _seleniumDriver;
+        private IWebElement _prevLink => _seleniumDriver.FindElement(By.Id("prev_link"));
+        #endregion
+
+        public TT_Create(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        #region methods
+
+        #endregion
     }
 }
