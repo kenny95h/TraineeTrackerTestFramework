6dda0f8237996d53ec5766e59df07e8a2686fa6d
boopidoop <daniel.summerside@yahoo.com>
2022-08-18 11:10:48 +0100

    Finished Trainees Index

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Trainees/TT_Index.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Trainees/TT_Index.cs
@@ -15,10 +15,10 @@ public class TT_Index
     #region Properties and Fields
     private IWebElement _spartaLogo => _seleniumDriver.FindElement(By.XPath("//img[@src='/assets/sparta-logo.png']"));
     private IWebElement _pageTitleHeader => _seleniumDriver.FindElement(By.ClassName("h1"));
-    private IWebElement _createNewTraineeLink => _seleniumDriver.FindElement(By.XPath("//a[@href='/Trainees/Create']"));
+    private IWebElement _createNewTraineeLink => _seleniumDriver.FindElement(By.Id("CreateLink"));
     private IWebElement _searchFilter => _seleniumDriver.FindElement(By.Id("SearchString"));
-    private IWebElement _submitFilter => _seleniumDriver.FindElement(By.XPath("//input[@value='Filter']"));
-    private IWebElement _resetFilter => _seleniumDriver.FindElement(By.XPath("//input[@value='Reset']"));
+    private IWebElement _submitFilter => _seleniumDriver.FindElement(By.Id("FilterString"));
+    private IWebElement _resetFilter => _seleniumDriver.FindElement(By.Id("ResetButton"));
     private List<IWebElement> _firstNames => _seleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).ToArray()[0].FindElements(By.TagName("td")).ToList();
     private List<IWebElement> _lastNames => _seleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).ToArray()[1].FindElements(By.TagName("td")).ToList();
     private List<IWebElement> _traineeTitles => _seleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).ToArray()[2].FindElements(By.TagName("td")).ToList();
@@ -29,38 +29,48 @@ public class TT_Index
     private List<IWebElement> _trackers => _seleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).ToArray()[7].FindElements(By.TagName("td")).ToList();
     private List<IWebElement> _consultantSkills => _seleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).ToArray()[8].FindElements(By.TagName("td")).ToList();
     private List<IWebElement> _technicalSkills => _seleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).ToArray()[9].FindElements(By.TagName("td")).ToList();
-    private Dictionary<string, Dictionary<string, IWebElement>> trainees;
+    private Dictionary<string, Dictionary<string, IWebElement>> _traineeDict;
+    private IWebElement _privacyPolicyLink => _seleniumDriver.FindElement(By.XPath("//a[@href='/Privacy']"));
     #endregion
 
     public TT_Index(WebDriver SeleniumDriver)
     {
         _seleniumDriver = SeleniumDriver;
-        trainees = new Dictionary<string, Dictionary<string, IWebElement>>();
+        _traineeDict = new Dictionary<string, Dictionary<string, IWebElement>>();
 
         for (int i = 0; i < _emailAddresses.Count; i++)
         {
-            trainees.Add(_emailAddresses[i].Text, new Dictionary<string, IWebElement>()
+            _traineeDict.Add(_emailAddresses[i].Text, new Dictionary<string, IWebElement>()
             {
                 {"first_name", _firstNames[i] },
                 {"last_name", _lastNames[i] },
+                {"last_name", _traineeTitles[i] },
                 {"contact_number", _contactNumbers[i] },
                 {"permission_role", _permissionRoles[i] },
                 {"profile_link", _profileLinks[i] },
                 {"tracker_edit", _trackers[i].FindElement(By.XPath($"//a[@href='/Trainees/Edit?id={i}']")) },
                 {"tracker_details", _trackers[i].FindElement(By.XPath($"//a[@href='/Trainees/Details?id={i}']")) },
                 {"tracker_delete", _trackers[i].FindElement(By.XPath($"//a[@href='/Trainees/Delete?id={i}']")) },
-                {"consultant_skills", _firstNames[i] },
-                {"technical_skills", _firstNames[i] }
+                {"consultant_skills", _consultantSkills[i] },
+                {"technical_skills", _technicalSkills[i] }
             });
         }
     }
 
     #region Methods
     public void ClickSpartaLogo() => _spartaLogo.Click();
+    public string GetPageTitle() => _seleniumDriver.Title;
+    public string GetTitleHeader() => _pageTitleHeader.Text;
     public void ClickCreateNew() => _createNewTraineeLink.Click();
     public string GetFilterText() => _searchFilter.Text;
     public void SetFilterText(string text) => _searchFilter.SendKeys(text);
     public void ClickSubmitFilter() => _submitFilter.Click();
     public void ClickResetFilter() => _resetFilter.Click();
+    public string FindEmailByUsername(string id) => _traineeDict.ContainsKey($"{id}@spartaglobal.com") ? $"{id}@spartaglobal.com" : "";
+    public string FindDetailByEmail(string email, string requestedDetail) => _traineeDict[email][requestedDetail].Text;
+    public void ClickEditByEmail(string email) => _traineeDict[email]["tracker_edit"].Click();
+    public void ClickDetailsByEmail(string email) => _traineeDict[email]["tracker_details"].Click();
+    public void ClickDeleteByEmail(string email) => _traineeDict[email]["tracker_delete"].Click();
+    public void ClickPrivacy() => _privacyPolicyLink.Click();
     #endregion
 }
