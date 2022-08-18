7cf4bb9edd3c1b599ea51c0dafed763cfe7a2ff3
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 12:26:22 +0100

    Commit all final changes to index & privacy pages

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Admin/TT_Index.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Admin/TT_Index.cs
@@ -9,7 +9,7 @@ namespace TraineeTrackerFramework.lib.pages.Admin
 {
     public class TT_Index
     {
-        #region Properties and Fields
+        #region Properties & Fields
         private IWebDriver _seleniumDriver;
         private string _indexPageURL = AppConfigReader.IndexURL;
 
@@ -18,11 +18,35 @@ namespace TraineeTrackerFramework.lib.pages.Admin
         private IWebElement _courses_collapse => _seleniumDriver.FindElement(By.Id("courses_collapse"));
         private IWebElement _account_collapse => _seleniumDriver.FindElement(By.Id("account_collapse"));
         private IWebElement _logout_button => _seleniumDriver.FindElement(By.Id("logout_button"));
+        private IWebElement _editTrainee => _seleniumDriver.FindElement(By.ClassName("trainees_edit_link"));
+        private IWebElement _viewDetailTrainee => _seleniumDriver.FindElement(By.ClassName("trainees_details_link"));
+        private IWebElement _deleteTrainee => _seleniumDriver.FindElement(By.ClassName("trainees_delete_link"));
+        private IWebElement _editTrainer => _seleniumDriver.FindElement(By.ClassName("trainers_edit_link"));
+        private IWebElement _viewDetailTrainer => _seleniumDriver.FindElement(By.ClassName("trainers_details_link"));
+        private IWebElement _deleteTrainer => _seleniumDriver.FindElement(By.ClassName("trainers_delete_link"));
+        private IWebElement _editCourse => _seleniumDriver.FindElement(By.ClassName("course_edit_link"));
+        private IWebElement _viewDetailCourse => _seleniumDriver.FindElement(By.ClassName("course_details_link"));
+        private IWebElement _deleteCourse => _seleniumDriver.FindElement(By.ClassName("course_delete_link"));
         #endregion
 
         public TT_Index(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
 
         #region Methods
+        public void CollapseTrainees() => _trainees_collapse.Click();
+        public void CollapseTrainers() => _trainers_collapse.Click();
+        public void CollapseCourses() => _courses_collapse.Click();
+        public void CollapseAccounts() => _account_collapse.Click();
+        public void LogOut() => _logout_button.Click();
+        // all the courses
+        public void EditTraineeLnk() => _editTrainee.Click();
+        public void EditTrainerLnk() => _editTrainer.Click();
+        public void EditCourseLnk() => _editTrainer.Click();
+        public void ViewTraineeLnk() => _viewDetailTrainee.Click();
+        public void ViewTrainerLnk() => _viewDetailTrainer.Click();
+        public void ViewCourseLnk() => _viewDetailCourse.Click();
+        public void DeleteTraineeLnk() => _deleteTrainee.Click();
+        public void DeleteTrainerLnk() => _deleteTrainer.Click();
+        public void DeleteCourseLnk() => _deleteCourse.Click();
         #endregion
     }
 }
