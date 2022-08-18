104c1491c5e1327b8d5871c99ccdc231d34b4d1e
Kenny <kharvey@spartaglobal.com>
2022-08-18 11:45:44 +0100

    Create method added, need to refactor call manager to take the json body

---
+++ TraineeTrackerFramework/APITestFramework/Tests/WhenTheTrainerServiceIsCalled_WithAValidTrainerToAdd.cs
@@ -0,0 +1,27 @@
+﻿using APITestApp;
+using APITestFramework.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestFramework.Tests
+{
+    public class WhenTheTrainerServiceIsCalled_WithAValidTrainerToAdd
+    {
+        readonly TrainerServices _trainerServices = new TrainerServices();
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUpAsync()
+        {
+            await _trainerServices.CreateRequestAsync("firstName:Thomas,lastName:Wolstencroft,title:Mr.,email:twolse@spartaglobal.com,permissionRole:Trainer", AppConfigReader.AdminAuth);
+        }
+
+        [Test]
+        public void NameOfTrainer_IsAllIWant()
+        {
+            Assert.That(_trainerServices.TrainerResponseDTO.Response.firstName, Is.EqualTo("Thomas"));
+        }
+    }
+}
