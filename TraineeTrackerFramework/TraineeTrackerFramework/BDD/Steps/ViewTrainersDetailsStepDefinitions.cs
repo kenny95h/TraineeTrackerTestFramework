cf567bcbe6584f6a19bda334882bc62174518a0f
EllisWitten <e.witten.2001@gmail.com>
2022-08-18 15:12:54 +0100

    add step defs

---
+++ TraineeTrackerFramework/TraineeTrackerFramework/BDD/Steps/ViewTrainersDetailsStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace TraineeTrackerFramework.BDD.Steps
+{
+    [Binding]
+    public class ViewTrainersDetailsStepDefinitions
+    {
+        [Given(@"I am logged in as an admin")]
+        public void GivenIAmLoggedInAsAnAdmin()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I am on the index page")]
+        public void GivenIAmOnTheIndexPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click on detials link")]
+        public void WhenIClickOnDetialsLink()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I land on the trianers detials page")]
+        public void ThenILandOnTheTrianersDetialsPage()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
