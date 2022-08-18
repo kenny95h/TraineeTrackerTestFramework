fb5d633f47321f0184fe1837adaa19304a4b64d3
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 17:24:33 +0100

    just for my branch edits

---
+++ TraineeTrackerFramework/TraineeTrackerFramework/BDD/Features/EditTrainer.feature
@@ -0,0 +1,51 @@
+﻿Feature: EditTrainer
+
+The page for editing trainer details.
+
+@EditForename
+Scenario: EditTrainersFirstName
+	Given I am logged in as admin
+	And I am on the index page
+	When I click edit trainer link
+	And I edit the trainers first name
+	Then The trainer's first name should be successfully updated
+
+@EditLastname
+Scenario: EditTrainersLastName
+	Given I am logged in as admin
+	And I am on the index page
+	When I click edit trainer link
+	And I edit the trainers last name
+	Then The trainer's last name should be successfully updated
+
+@EditTitle
+Scenario: EditTrainersTitle
+	Given I am logged in as admin
+	And I am on the index page
+	When I click edit trainer link
+	And I edit the trainers title
+	Then The trainer's title should be successfully updated
+
+@EditEmail
+Scenario: EditTrainersEmail
+	Given I am logged in as admin
+	And I am on the index page
+	When I click edit trainer link
+	And I edit the trainers email
+	Then The trainer's email should be successfully updated
+
+@EditContactNo
+Scenario: EditTrainersContactNo
+	Given I am logged in as admin
+	And I am on the index page
+	When I click edit trainer link
+	And I edit the trainers contact number
+	Then The trainer's contact number should be successfully updated
+
+@EditContactNo
+Scenario: EditTrainersPermissions
+	Given I am logged in as an admin
+	And I am on the index page
+	When I click edit trainer link
+	And I edit the trainers permission level
+	Then The trainer's permission role should be successfully updated
---
+++ TraineeTrackerFramework/TraineeTrackerFramework/BDD/Steps/EditTrainerStepDefinitions.cs
@@ -0,0 +1,93 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace TraineeTrackerFramework.BDD.Steps
+{
+    [Binding]
+    public class EditTrainerStepDefinitions
+    {
+        [Given(@"I am logged in as admin")]
+        public void GivenIAmLoggedInAsAdmin()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click edit trainer link")]
+        public void WhenIClickEditTrainerLink()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I edit the trainers first name")]
+        public void WhenIEditTheTrainersFirstName()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The trainer's first name should be successfully updated")]
+        public void ThenTheTrainersFirstNameShouldBeSuccessfullyUpdated()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I edit the trainers last name")]
+        public void WhenIEditTheTrainersLastName()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The trainer's last name should be successfully updated")]
+        public void ThenTheTrainersLastNameShouldBeSuccessfullyUpdated()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I edit the trainers title")]
+        public void WhenIEditTheTrainersTitle()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The trainer's title should be successfully updated")]
+        public void ThenTheTrainersTitleShouldBeSuccessfullyUpdated()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I edit the trainers email")]
+        public void WhenIEditTheTrainersEmail()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The trainer's email should be successfully updated")]
+        public void ThenTheTrainersEmailShouldBeSuccessfullyUpdated()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I edit the trainers contact number")]
+        public void WhenIEditTheTrainersContactNumber()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The trainer's contact number should be successfully updated")]
+        public void ThenTheTrainersContactNumberShouldBeSuccessfullyUpdated()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I edit the trainers permission level")]
+        public void WhenIEditTheTrainersPermissionLevel()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The trainer's permission role should be successfully updated")]
+        public void ThenTheTrainersPermissionRoleShouldBeSuccessfullyUpdated()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
