e153fb05b7363f5b24d094319df6688d9a763221
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 12:21:33 +0100

    Finallised my assigned POM Pages

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Account/TT_AccessDenied.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Account/TT_AccessDenied.cs
@@ -10,16 +10,16 @@ namespace TraineeTrackerFramework.lib.pages.Account
     public class TT_AccessDenied
     {
         #region properties & fields
-        IWebDriver _seleniumDriver;
-        IWebElement _homeLink => _seleniumDriver.FindElement(By.Id("home_link"));
-        IWebElement _prevLink => _seleniumDriver.FindElement(By.Id("prev_link"));
+        private IWebDriver _seleniumDriver;
+        private IWebElement _backBtn => _seleniumDriver.FindElement(By.Id("prev_link"));
+        private IWebElement _homeLink => _seleniumDriver.FindElement(By.Id("home_link"));
         #endregion
 
         public TT_AccessDenied(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
 
         #region methods
-        public void ReturnHome() => _homeLink.Click();
-        public void StepBack() => _prevLink.Click();
+        public void ReturnBack() => _backBtn.Click();
+        public void HomePage() => _homeLink.Click();
         public string GetPageTitle() => _seleniumDriver.Title;
         #endregion
     }
