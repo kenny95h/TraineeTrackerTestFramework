85309b99cd62617df94e08f1daa4a611a8229427
SupaDylan11 <91839242+SupaDylan11@users.noreply.github.com>
2022-08-18 12:32:01 +0100

    Restore TTAccessDenied

--- TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Account/TT_AccessDenied.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/lib/pages/Account/TT_AccessDenied.cs
@@ -9,5 +9,18 @@ namespace TraineeTrackerFramework.lib.pages.Account
 {
     public class TT_AccessDenied
     {
+        #region properties & fields
+        IWebDriver _seleniumDriver;
+        IWebElement _homeLink => _seleniumDriver.FindElement(By.Id("home_link"));
+        IWebElement _prevLink => _seleniumDriver.FindElement(By.Id("prev_link"));
+        #endregion
+
+        public TT_AccessDenied(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+
+        #region methods
+        public void ReturnHome() => _homeLink.Click();
+        public void StepBack() => _prevLink.Click();
+        public string GetPageTitle() => _seleniumDriver.Title;
+        #endregion
     }
 }
