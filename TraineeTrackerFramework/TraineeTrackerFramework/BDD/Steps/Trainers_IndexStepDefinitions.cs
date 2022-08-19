479b0b687f87a486ad0f89769bd25c839f4d47de
boopidoop <daniel.summerside@yahoo.com>
2022-08-19 12:05:50 +0100

    Automated Index Page Tests (Courses, Trainees and Trainers). Trainees tests don't work due to Selenium defect

--- TraineeTrackerFramework/TraineeTrackerFramework/BDD/Steps/Trainers_IndexStepDefinitions.cs
+++ TraineeTrackerFramework/TraineeTrackerFramework/BDD/Steps/Trainers_IndexStepDefinitions.cs
@@ -1,21 +1,42 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using TechTalk.SpecFlow;
+using TraineeTrackerFramework.lib;
 
 namespace TraineeTrackerFramework.BDD.Steps
 {
     [Binding]
     public class Trainers_IndexStepDefinitions
     {
+        TT_Website<ChromeDriver> TT_Website;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            TT_Website = new TT_Website<ChromeDriver>();
+            TT_Website.SeleniumDriver.Manage().Window.Maximize();
+        }
+
         [When(@"I navigate to the Trainers page")]
         public void WhenINavigateToTheTrainersPage()
         {
-            throw new PendingStepException();
+            TT_Website.SeleniumDriver.FindElement(By.ClassName("trainers_details_link"));
+            TT_Website.SeleniumDriver.FindElement(By.Id("prev_link"));
         }
 
         [Then(@"I should see a list of all Trainers")]
         public void ThenIShouldSeeAListOfAllTrainers()
         {
-            throw new PendingStepException();
+            Assert.That(TT_Website.SeleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr")).Count, Is.GreaterThan(0));
+            Assert.That(TT_Website.SeleniumDriver.FindElement(By.ClassName("table")).FindElements(By.TagName("tr"))[0].FindElements(By.TagName("td"))[5].Text, Does.Contain("Trainee"));
+        }
+
+        [AfterScenario]
+        public void Teardown()
+        {
+            TT_Website.SeleniumDriver.Quit();
         }
     }
 }
